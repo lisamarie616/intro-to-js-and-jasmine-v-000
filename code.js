14636a8b5f098cdf46c35b658b906f13cab1559d
lisamarie616 <lisamarie616@gmail.com>
2016-03-15 12:16:48 -0700

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function favIceCream(name){
+  return "I love " + name;
+}
+
+function shouting(string){
+  return string.toUpperCase();
+}
+
+function roundDown(number){
+  return Math.floor(number);
+}
+
+function theTruth(){
+  return true;
+}
